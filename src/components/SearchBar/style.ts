9e842ecf33353f63dceb0635eaf551e4a1a276b0
Filipe Melo <filipemelo032@gmail.com>
2021-04-10 16:56:31 -0300

    Created Search bar component

---
+++ src/components/SearchBar/style.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 419px;
+`;
+
+export const Button = styled.button`
+  border: 0;
+  padding: 0px 12px 0px 12px;
+  display: flex;
+  height: 100%;
+  background-color: transparent;
+  align-items: center;
+  justify-content: center;
+`;
