cbfa58522d276aefefbb872af05f13c716cfdd3b
Filipe Melo <filipemelo032@gmail.com>
2021-04-10 20:20:56 -0300

    Calling List Component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Content } from './styles';
 import NavBar from '../../components/NavBar';
-
+import RepositoryList from '../../components/RepositoryList';
 const Home: React.FC = () => {
   return (
     <Container>
       <NavBar />
+      <Content>
+        <RepositoryList />
+      </Content>
     </Container>
   );
 };
