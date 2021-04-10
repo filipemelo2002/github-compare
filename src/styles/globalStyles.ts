4f3da2bfad7557b76f78b335ee9603ad05045cb4
Filipe Melo <filipemelo032@gmail.com>
2021-04-10 16:57:17 -0300

    Added new styles

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -12,12 +12,24 @@ export default createGlobalStyle`
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
-  
+
+  ul {
+    list-style: none;
+    margin: 0;
+  }
+  #root {
+    position: relative;
+  }
   code {
     font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
       monospace;
   }
+
+  --secondary: #6B6C7E;
+  --primary: #0B5FFF;
+  --dark: #272833;
+  --warning: #B95000;
 `;
