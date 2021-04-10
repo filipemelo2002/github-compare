3e69b1eb1114a8eddd1cbd362737414d3c9e1bf0
Filipe Melo <filipemelo032@gmail.com>
2021-04-10 16:55:26 -0300

    Added Dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Home from './pages/Home';
 import GlobalStyle from './styles/globalStyles';
-
+import '@clayui/css/lib/css/atlas.css';
 const App: React.FC = () => (
   <>
     <Home />
