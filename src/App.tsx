e36aa0b24853c79dafbc2f54fd4bdb6620939a87
Filipe Melo <filipemelo032@gmail.com>
2021-04-13 00:47:27 -0300

    Added Redux provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Home from './pages/Home';
 import GlobalStyle from './styles/globalStyles';
 import '@clayui/css/lib/css/atlas.css';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 const App: React.FC = () => (
-  <>
+  <Provider store={store}>
     <Home />
     <GlobalStyle />
-  </>
+  </Provider>
 );
 
 export default App;
