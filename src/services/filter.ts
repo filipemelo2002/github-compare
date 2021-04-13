78f71b6827c06efea0561b37f09587645c577682
Filipe Melo <filipemelo032@gmail.com>
2021-04-12 23:08:59 -0300

    creating filter functions

---
+++ src/services/filter.ts
@@ -0,0 +1,18 @@
+const filterStars = () => {};
+
+const filterForks = () => {};
+
+const filterOpenIssues = () => {};
+
+const filterAge = () => {};
+
+const filterLastCommit = () => {};
+
+const filters = {
+  stars: filterStars,
+  forks: filterForks,
+  openIssues: filterOpenIssues,
+  age: filterAge,
+  lastCommit: filterLastCommit,
+};
+export default filters;
